800de3adc1df515355338bd99767e7de241982a5
Bryant Facenda <bryantffacen@gmail.com>
2025-11-06 22:28:58 -0400

    add index.html and main.js
    Tarea realizada exitosamente

---
+++ main.js
@@ -0,0 +1,31 @@
+//1. Create two variables, assign them numeric values, and print their multiplication result.
+let number1 = 5 
+let number2 = 6 
+let result = number1 * number2
+console.log(result)
+
+// 2. Create a variable to store your age and print a message using template literals that includes your age.
+let firstName = "Bryant"
+let lastName = "Facenda"
+let age = 22 
+console.log(`Hi, my name is ${firstName} ${lastName} and i am ${age} years old`)
+
+//3. Create three boolean variables and use logical operators to print the result of, at least, two different conditions.
+let iamHunger = true
+let IamGoing = false
+let IWantCoffee = false
+let result1 = iamHunger && IamGoing
+let result2 = iamHunger || IamGoing
+let result3 = !IWantCoffee
+console.log(result1)
+console.log(result2)
+console.log(result3)
+
+//4. Create a little script that initialize a numeric variable, then increment it by 1, and finally, compare it with the value of pi (3.14).
+let x = 4
+x++
+const pi = 3.14
+
+console.log(x <= pi)
+
+//End of the code
